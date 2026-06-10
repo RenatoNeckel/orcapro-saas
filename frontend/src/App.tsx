7a1fc16f9c58535a779f031bb961e9afc4c80320
Renato Neckel <renatoneckeldev@gmail.com>
2026-06-10 13:10:03 -0300

    feat: configura rotas principais da aplicacao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import ConfiguracoesEmpresa from "./pages/ConfiguracoesEmpresa";
+import Clientes from "./pages/Clientes";
+import Orcamentos from "./pages/Orcamentos";
 
 function App() {
-  return <ConfiguracoesEmpresa />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/empresa" replace />} />
+
+      <Route path="/empresa" element={<ConfiguracoesEmpresa />} />
+
+      <Route path="/clientes" element={<Clientes />} />
+
+      <Route path="/orcamentos" element={<Orcamentos />} />
+    </Routes>
+  );
 }
 
 export default App;
