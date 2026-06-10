002ff13bcb73f6b1a4f512c0afbedafe522649e1
Renato Neckel <renatoneckeldev@gmail.com>
2026-06-10 13:04:37 -0300

    feat: adiciona paginas iniciais de clientes e orcamentos

---
+++ frontend/src/pages/Clientes.tsx
@@ -0,0 +1,10 @@
+function Clientes() {
+  return (
+    <div>
+      <h1>Clientes</h1>
+      <p>Cadastro de clientes em desenvolvimento.</p>
+    </div>
+  );
+}
+
+export default Clientes;
---
+++ frontend/src/pages/Orçamentos.tsx
@@ -0,0 +1,10 @@
+function Orcamentos() {
+  return (
+    <div>
+      <h1>Orçamentos</h1>
+      <p>Tela de orçamentos em desenvolvimento.</p>
+    </div>
+  );
+}
+
+export default Orcamentos;
